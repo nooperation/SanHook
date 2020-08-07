29c007064fe5151ae7199d33c42486295e869809
nooperation <madbrahmin@gmail.com>
2020-08-07 02:01:08 -0400

    refactoring gameworld

--- SanHook/GameWorld.h
+++ SanHook/GameWorld.h
@@ -33,7 +33,7 @@ public:
                 GameWorld::OnTimestamp(reader);
                 break;
             }
-            case GameWorldMessages::MoveEntity: // 0xEFC20B7F  // 16E9E30
+            case GameWorldMessages::MoveEntity: // 16E9E30
             {
                 GameWorld::OnMoveEntity(reader);
                 break;
@@ -92,51 +92,48 @@ public:
         return true;
     }
 
-    static void OnUpdateRuntimeInventorySettings(PacketReader &reader)
+    static void OnTimestamp(PacketReader &reader) // 16E9DC0
     {
-        auto spawnSource = reader.ReadUint8();
-        auto spawnLifetimePolicy = reader.ReadUint8();
-        auto totalSpawnLimit = reader.ReadUint16();
-        auto perUserSpawnLimit = reader.ReadUint16();
-        auto spawnTimeout = reader.ReadUint32();
-    }
-
-    static void OnScriptCameraMessage(PacketReader &reader)
-    {
-        auto componentid = reader.ReadUint64();
+        auto nanoseconds = reader.ReadUint64();
         auto frame = reader.ReadUint64();
-        auto controlMode = reader.ReadBits(4);
     }
 
-    static void OnRiggedMeshScaleChanged(PacketReader &reader)
+    static void OnMoveEntity(PacketReader &reader)// 16E9E30
     {
-        auto componentid = reader.ReadUint64();
-        auto frame = reader.ReadUint64();
-        auto scale = reader.ReadFloat();
+        auto startFrame = reader.ReadUint64();
+        auto componentId = reader.ReadUint64();
+        auto startPosition = reader.ReadVectorF(3);
+        auto targetPosition = reader.ReadVectorF(3);
+        auto time = reader.ReadUint32();
+        //auto startOrientation = reader.ReadBits(46);
+        //auto targetOrientation = reader.ReadBits(46);
+        //auto positionInterpMode = reader.ReadBits(4);
+        //auto rotationInterpMode = reader.ReadBits(4);
+
+        //printf("GameWorldMessages::MoveEntity:\n  startFrame = %llu\n  componentId = %llu\n startPosition = <%f, %f, %f>\n  targetPosition = <%f, %f, %f>\n  time = %u\n",
+        //    startFrame,
+        //    componentId,
+        //    startPosition[0], startPosition[1], startPosition[2],
+        //    targetPosition[0], targetPosition[1], targetPosition[2],
+        //    time
+        //);
     }
 
-    static void OnRiggedMeshFlagsChange(PacketReader &reader)
+    static void OnChangeMaterialVectorParam(PacketReader &reader)// 16E9EA0
     {
-        auto componentid = reader.ReadUint64();
-        auto frame = reader.ReadUint64();
-        auto flags = reader.ReadUint8();
+        auto parameter = reader.ReadUint8();
+        auto start = reader.ReadVectorF(3);
+        auto end = reader.ReadVectorF(3);
     }
 
-    static void OnStaticMeshScaleChanged(PacketReader &reader)
+    static void OnChangeMaterialFloatParam(PacketReader &reader)   // 16E9F10
     {
-        auto componentid = reader.ReadUint64();
-        auto frame = reader.ReadUint64();
-        auto scale = reader.ReadFloat();
+        auto parameter = reader.ReadUint8();
+        auto start = reader.ReadFloat();
+        auto end = reader.ReadFloat();
     }
 
-    static void OnStaticMeshFlagsChanged(PacketReader &reader)
-    {
-        auto componentid = reader.ReadUint64();
-        auto frame = reader.ReadUint64();
-        auto flags = reader.ReadUint8();
-    }
-
-    static void OnChangeMaterial(PacketReader &reader)
+    static void OnChangeMaterial(PacketReader &reader) // 16E9F80
     {
         auto startFrame = reader.ReadUint64();
         auto componentId = reader.ReadUint64();
@@ -159,44 +156,47 @@ public:
         */
     }
 
-    static void OnChangeMaterialFloatParam(PacketReader &reader)
+    static void OnStaticMeshFlagsChanged(PacketReader &reader) // 16E9FF0
     {
-        auto parameter = reader.ReadUint8();
-        auto start = reader.ReadFloat();
-        auto end = reader.ReadFloat();
-    }
-
-    static void OnChangeMaterialVectorParam(PacketReader &reader)
-    {
-        auto parameter = reader.ReadUint8();
-        auto start = reader.ReadVectorF(3);
-        auto end = reader.ReadVectorF(3);
-    }
-
-    static void OnMoveEntity(PacketReader &reader)
-    {
-        auto startFrame = reader.ReadUint64();
-        auto componentId = reader.ReadUint64();
-        auto startPosition = reader.ReadVectorF(3);
-        auto targetPosition = reader.ReadVectorF(3);
-        auto time = reader.ReadUint32();
-        //auto startOrientation = reader.ReadBits(46);
-        //auto targetOrientation = reader.ReadBits(46);
-        //auto positionInterpMode = reader.ReadBits(4);
-        //auto rotationInterpMode = reader.ReadBits(4);
-
-        //printf("GameWorldMessages::MoveEntity:\n  startFrame = %llu\n  componentId = %llu\n startPosition = <%f, %f, %f>\n  targetPosition = <%f, %f, %f>\n  time = %u\n",
-        //    startFrame,
-        //    componentId,
-        //    startPosition[0], startPosition[1], startPosition[2],
-        //    targetPosition[0], targetPosition[1], targetPosition[2],
-        //    time
-        //);
-    }
-
-    static void OnTimestamp(PacketReader &reader)
-    {
-        auto nanoseconds = reader.ReadUint64();
+        auto componentid = reader.ReadUint64();
         auto frame = reader.ReadUint64();
+        auto flags = reader.ReadUint8();
+    }
+
+    static void OnStaticMeshScaleChanged(PacketReader &reader)  // 16EA060
+    {
+        auto componentid = reader.ReadUint64();
+        auto frame = reader.ReadUint64();
+        auto scale = reader.ReadFloat();
+    }
+
+    static void OnRiggedMeshFlagsChange(PacketReader &reader) // 16EA0D0
+    {
+        auto componentid = reader.ReadUint64();
+        auto frame = reader.ReadUint64();
+        auto flags = reader.ReadUint8();
+    }
+
+    static void OnRiggedMeshScaleChanged(PacketReader &reader) // 16EA140
+    {
+        auto componentid = reader.ReadUint64();
+        auto frame = reader.ReadUint64();
+        auto scale = reader.ReadFloat();
+    }
+
+    static void OnScriptCameraMessage(PacketReader &reader) // 16EA1B0
+    {
+        auto componentid = reader.ReadUint64();
+        auto frame = reader.ReadUint64();
+        auto controlMode = reader.ReadBits(4);
+    }
+
+    static void OnUpdateRuntimeInventorySettings(PacketReader &reader) // 16EA220
+    {
+        auto spawnSource = reader.ReadUint8();
+        auto spawnLifetimePolicy = reader.ReadUint8();
+        auto totalSpawnLimit = reader.ReadUint16();
+        auto perUserSpawnLimit = reader.ReadUint16();
+        auto spawnTimeout = reader.ReadUint32();
     }
 };
